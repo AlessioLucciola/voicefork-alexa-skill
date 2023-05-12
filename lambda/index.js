1ef5e3f28c6c01248e6b7199ff7f9de5d6e15acf
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2023-05-12 18:19:10 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -19,6 +19,21 @@ const LaunchRequestHandler = {
     }
 };
 
+const MakeReservationIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'MakeReservationIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Hi, tell me the details of the reservation!';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+}
+
 const HelloWorldIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
