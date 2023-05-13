3bd1e6dbdb1b747d5d1d89e662139e966dd48671
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2023-05-13 06:37:03 +0000

    updated lambda/customIntents.js.

--- lambda/customIntents.js
+++ lambda/customIntents.js
@@ -2,10 +2,17 @@ const Alexa = require('ask-sdk-core');
 const { format } = require('date-fns');
 
 const MakeReservationIntentHandler = {
+    
     canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'MakeReservationIntent';
-    },
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest' &&
+      request.intent.name === 'PlanMyTripIntent' &&
+      request.dialogState === 'IN_PROGRESS';
+  },
+    // canHandle(handlerInput) {
+    //     return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+    //         && Alexa.getIntentName(handlerInput.requestEnvelope) === 'MakeReservationIntent';
+    // },
     handle(handlerInput) {
     const { request } = handlerInput.requestEnvelope;
     const { intent } = request;
