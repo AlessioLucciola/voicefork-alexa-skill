65732dfb515f749ff1cd7dfc3253350ef7da5f02
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2023-05-13 09:54:12 +0200

    Small changes

--- lambda/IntentHandlers/MakeReservationIntent.js
+++ lambda/IntentHandlers/MakeReservationIntent.js
@@ -1,69 +1,59 @@
-const Alexa = require('ask-sdk-core');
-const { format } = require('date-fns');
+const Alexa = require("ask-sdk-core")
+const { format } = require("date-fns")
+
+//Test
 
 const StaredtMakeReservationIntentHandler = {
-    canHandle(handlerInput) {
-        const request = handlerInput.requestEnvelope.request;
-        return request.type === 'IntentRequest' &&
-          request.intent.name === 'MakeReservationIntent' &&
-          request.dialogState === 'STARTED';
-        },
-    // canHandle(handlerInput) {
-    //     return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-    //         && Alexa.getIntentName(handlerInput.requestEnvelope) === 'MakeReservationIntent';
-    // },
-    handle(handlerInput) {
-    const currentIntent = handlerInput.requestEnvelope.request.intent;       
+	canHandle(handlerInput) {
+		const request = handlerInput.requestEnvelope.request
+		return request.type === "IntentRequest" && request.intent.name === "MakeReservationIntent" && request.dialogState === "STARTED"
+	},
+	// canHandle(handlerInput) {
+	//     return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+	//         && Alexa.getIntentName(handlerInput.requestEnvelope) === 'MakeReservationIntent';
+	// },
+	handle(handlerInput) {
+		const currentIntent = handlerInput.requestEnvelope.request.intent
 
-    // const speakOutput = 'Tell me the details of the reservation.';
-    return handlerInput.responseBuilder
-        .addDelegateDirective(currentIntent)
-        .getResponse();
-  }
-};
+		// const speakOutput = 'Tell me the details of the reservation.';
+		return handlerInput.responseBuilder.addDelegateDirective(currentIntent).getResponse()
+	},
+}
 
 const InProgressMakeReservationIntentHandler = {
-    canHandle(handlerInput) {
-        const request = handlerInput.requestEnvelope.request;
-        return request.type === 'IntentRequest' &&
-          request.intent.name === 'MakeReservationIntent' &&
-          request.dialogState === 'IN_PROGRESS';
-        },
-    // canHandle(handlerInput) {
-    //     return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-    //         && Alexa.getIntentName(handlerInput.requestEnvelope) === 'MakeReservationIntent';
-    // },
-    handle(handlerInput) {
-    const currentIntent = handlerInput.requestEnvelope.request.intent;       
-    currentIntent.slots.restaurantName = "mariettone"
-    // const speakOutput = 'Reservation in progress';
-    return handlerInput.responseBuilder
-        .addDelegateDirective(currentIntent)
-        .getResponse();
-  }
-};
+	canHandle(handlerInput) {
+		const request = handlerInput.requestEnvelope.request
+		return request.type === "IntentRequest" && request.intent.name === "MakeReservationIntent" && request.dialogState === "IN_PROGRESS"
+	},
+	// canHandle(handlerInput) {
+	//     return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+	//         && Alexa.getIntentName(handlerInput.requestEnvelope) === 'MakeReservationIntent';
+	// },
+	handle(handlerInput) {
+		const currentIntent = handlerInput.requestEnvelope.request.intent
+		currentIntent.slots.restaurantName = "mariettone"
+		// const speakOutput = 'Reservation in progress';
+		return handlerInput.responseBuilder.addDelegateDirective(currentIntent).getResponse()
+	},
+}
 
 const CompletedMakeReservationIntentHandler = {
-    canHandle(handlerInput) {
-        const request = handlerInput.requestEnvelope.request;
-        return request.type === 'IntentRequest' &&
-          request.intent.name === 'MakeReservationIntent' &&
-          request.dialogState === 'COMPLETED';
-        },
-    // canHandle(handlerInput) {
-    //     return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-    //         && Alexa.getIntentName(handlerInput.requestEnvelope) === 'MakeReservationIntent';
-    // },
-    handle(handlerInput) {
-    const speakOutput = 'Reservation completed.';
-    return handlerInput.responseBuilder
-        .speak(speakOutput)
-        .getResponse();
-  }
-};
+	canHandle(handlerInput) {
+		const request = handlerInput.requestEnvelope.request
+		return request.type === "IntentRequest" && request.intent.name === "MakeReservationIntent" && request.dialogState === "COMPLETED"
+	},
+	// canHandle(handlerInput) {
+	//     return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+	//         && Alexa.getIntentName(handlerInput.requestEnvelope) === 'MakeReservationIntent';
+	// },
+	handle(handlerInput) {
+		const speakOutput = "Reservation completed."
+		return handlerInput.responseBuilder.speak(speakOutput).getResponse()
+	},
+}
 
 module.exports = {
-    StaredtMakeReservationIntentHandler,
-    InProgressMakeReservationIntentHandler, 
-    CompletedMakeReservationIntentHandler
+	StaredtMakeReservationIntentHandler,
+	InProgressMakeReservationIntentHandler,
+	CompletedMakeReservationIntentHandler,
 }
