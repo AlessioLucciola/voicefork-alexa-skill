ff944f5b469ce978a0b3c2989a925ef40f280abe
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2023-05-13 14:30:09 +0200

    Update

--- lambda/IntentHandlers/MakeReservationIntent.js
+++ lambda/IntentHandlers/MakeReservationIntent.js
@@ -1,70 +1,63 @@
 const Alexa = require("ask-sdk-core")
 const { format } = require("date-fns")
 
-const StartedMakeReservationIntentHandler = {
+const MakeReservationIntentHandler = {
 	canHandle(handlerInput) {
 		const { dialogState, type } = handlerInput.requestEnvelope.request
 		const { name } = handlerInput.requestEnvelope.request.intent
-		return type === "IntentRequest" && name === "MakeReservationIntent" && dialogState === "STARTED"
+		return type === "IntentRequest" && name === "MakeReservationIntent"
 	},
 	handle(handlerInput) {
 		const currentIntent = handlerInput.requestEnvelope.request.intent
-		return handlerInput.responseBuilder.speak("Starting_2,").reprompt("Again starting, ").addDelegateDirective(currentIntent).getResponse()
+
+		const { resturantName, date, time, numPeople } = currentIntent.slots
+
+		if (!resturantName || !date || !time || !numPeople) return handlerInput.responseBuilder.speak("Missing parameters,").reprompt("Again missing parameters,").addDelegateDirective().getResponse()
+
+		if (resturantName != "marione")
+			return handlerInput.responseBuilder.speak("You have to say marione!").reprompt("Again, You have to say marione!").addElicitSlotDirective("restaurantName").getResponse()
+
+		return handlerInput.responseBuilder.speak(`Final reservation details: ${restaurantName}, ${date}, ${time}, ${numPeople}`).withShouldEndSession(true).getResponse()
 	},
 }
 
-const InProgressMakeReservationIntentHandler = {
-	canHandle(handlerInput) {
-		const { dialogState, type } = handlerInput.requestEnvelope.request
-		const { name } = handlerInput.requestEnvelope.request.intent
-		return type === "IntentRequest" && name === "MakeReservationIntent" && dialogState === "IN_PROGRESS"
-	},
-	handle(handlerInput) {
-		const currentIntent = handlerInput.requestEnvelope.request.intent
-		return handlerInput.responseBuilder.speak("In progress,").reprompt("Again in progress, ").addDelegateDirective(currentIntent).getResponse()
-	},
-}
+// const VerifyRestaurantNameReservationHandler = {
+// 	canHandle(handlerInput) {
+// 		// const { dialogState, type } = handlerInput.requestEnvelope.request
+// 		// const { name } = handlerInput.requestEnvelope.request.intent
+// 		// let restaurantName
+// 		// if (type === "IntentRequest") restaurantName = handlerInput.requestEnvelope.request?.intent?.slots?.restaurantName
+// 		// return type === "IntentRequest" && name === "MakeReservationIntent" && restaurantName !== undefined
+// 		return (
+// 			handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+// 			handlerInput.requestEnvelope.request.intent.name === "MakeReservationIntent" &&
+// 			handlerInput.requestEnvelope.request.intent.slots.restaurantName.value
+// 		)
+// 	},
+// 	handle(handlerInput) {
+// 		const currentIntent = handlerInput.requestEnvelope.request.intent
+// 		const { restaurantName } = currentIntent.slots
+// 		if (restaurantName.value == "marione") return handlerInput.responseBuilder.speak("Ok, marioneeeee").addDelegateDirective(currentIntent).getResponse()
 
-const VerifyRestaurantNameReservationHandler = {
-	canHandle(handlerInput) {
-		// const { dialogState, type } = handlerInput.requestEnvelope.request
-		// const { name } = handlerInput.requestEnvelope.request.intent
-		// let restaurantName
-		// if (type === "IntentRequest") restaurantName = handlerInput.requestEnvelope.request?.intent?.slots?.restaurantName
-		// return type === "IntentRequest" && name === "MakeReservationIntent" && restaurantName !== undefined
-		return (
-			handlerInput.requestEnvelope.request.type === "IntentRequest" &&
-			handlerInput.requestEnvelope.request.intent.name === "MakeReservationIntent" &&
-			handlerInput.requestEnvelope.request.intent.slots.restaurantName.value
-		)
-	},
-	handle(handlerInput) {
-		const currentIntent = handlerInput.requestEnvelope.request.intent
-		const { restaurantName } = currentIntent.slots
-		if (restaurantName.value == "marione") return handlerInput.responseBuilder.speak("Ok, marioneeeee").addDelegateDirective(currentIntent).getResponse()
+// 		return handlerInput.responseBuilder
+// 			.speak("The restaurant name is not valid (you have to say marione!) What is the name of the place?")
+// 			.addElicitSlotDirective("restaurantName", currentIntent)
+// 			.getResponse()
+// 	},
+// }
 
-		return handlerInput.responseBuilder
-			.speak("The restaurant name is not valid (you have to say marione!) What is the name of the place?")
-			.addElicitSlotDirective("restaurantName", currentIntent)
-			.getResponse()
-	},
-}
-
-const CompletedMakeReservationIntentHandler = {
-	canHandle(handlerInput) {
-		const { dialogState, type } = handlerInput.requestEnvelope.request
-		const { name } = handlerInput.requestEnvelope.request.intent
-		return type === "IntentRequest" && name === "MakeReservationIntent" && dialogState === "COMPLETED"
-	},
-	handle(handlerInput) {
-		const speakOutput = "Reservation completed."
-		return handlerInput.responseBuilder.speak(speakOutput).withShouldEndSession(true).getResponse()
-	},
-}
+// const CompletedMakeReservationIntentHandler = {
+// 	canHandle(handlerInput) {
+// 		const { dialogState, type } = handlerInput.requestEnvelope.request
+// 		const { name } = handlerInput.requestEnvelope.request.intent
+// 		return type === "IntentRequest" && name === "MakeReservationIntent" && dialogState === "COMPLETED"
+// 	},
+// 	handle(handlerInput) {
+// 		const speakOutput = "Reservation completed."
+// 		return handlerInput.responseBuilder.speak(speakOutput).withShouldEndSession(true).getResponse()
+// 	},
+// }
 
 module.exports = {
-	StartedMakeReservationIntentHandler,
-	InProgressMakeReservationIntentHandler,
-	VerifyRestaurantNameReservationHandler,
-	CompletedMakeReservationIntentHandler,
+	MakeReservationIntentHandler,
 }
--- lambda/index.js
+++ lambda/index.js
@@ -1,12 +1,7 @@
 const Alexa = require("ask-sdk-core")
 const { LaunchRequestHandler, CancelAndStopIntentHandler, FallbackIntentHandler, HelpIntentHandler } = require("./IntentHandlers/defaultIntents")
 const { SessionEndedRequestHandler, IntentReflectorHandler, ErrorHandler } = require("./handlers")
-const {
-	StartedMakeReservationIntentHandler,
-	InProgressMakeReservationIntentHandler,
-	VerifyRestaurantNameReservationHandler,
-	CompletedMakeReservationIntentHandler,
-} = require("./IntentHandlers/MakeReservationIntent")
+const { MakeReservationIntentHandler } = require("./IntentHandlers/MakeReservationIntent")
 
 /**
  * This handler acts as the entry point for your skill, routing all request and response
@@ -14,18 +9,7 @@ const {
  * defined are included below. The order matters - they're processed top to bottom
  * */
 exports.handler = Alexa.SkillBuilders.custom()
-	.addRequestHandlers(
-		LaunchRequestHandler,
-		// VerifyRestaurantNameReservationHandler,
-		StartedMakeReservationIntentHandler,
-		InProgressMakeReservationIntentHandler,
-		CompletedMakeReservationIntentHandler,
-		HelpIntentHandler,
-		CancelAndStopIntentHandler,
-		FallbackIntentHandler,
-		SessionEndedRequestHandler,
-		IntentReflectorHandler
-	)
+	.addRequestHandlers(LaunchRequestHandler, MakeReservationIntentHandler, HelpIntentHandler, CancelAndStopIntentHandler, FallbackIntentHandler, SessionEndedRequestHandler, IntentReflectorHandler)
 	.addErrorHandlers(ErrorHandler)
 	.withApiClient(new Alexa.DefaultApiClient())
 	.withCustomUserAgent("sample/hello-world/v1.2")
