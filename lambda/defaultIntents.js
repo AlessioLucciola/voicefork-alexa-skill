f17359e69a6fc35f7197bc6128b1a89da757edfe
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2023-05-13 05:57:17 +0000

    updated lambda/defaultIntents.js.

--- lambda/defaultIntents.js
+++ lambda/defaultIntents.js
@@ -84,5 +84,6 @@ module.exports = {
     LaunchRequestHandler,
     HelloWorldIntentHandler,
     CancelAndStopIntentHandler,
-    FallbackIntentHandler
+    FallbackIntentHandler,
+    HelpIntentHandler
 }
