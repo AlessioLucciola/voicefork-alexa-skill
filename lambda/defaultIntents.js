ac85d4e9bbbc7410b36ff8aaeccaed88aa0d9c9c
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2023-05-13 05:57:13 +0000

    updated lambda/defaultIntents.js.

--- lambda/defaultIntents.js
+++ lambda/defaultIntents.js
@@ -64,6 +64,21 @@ const FallbackIntentHandler = {
     }
 };
 
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'You can say hello to me! How can I help?';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
 
 module.exports = {
     LaunchRequestHandler,
