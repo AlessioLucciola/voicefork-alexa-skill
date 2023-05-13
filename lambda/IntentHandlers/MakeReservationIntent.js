cb70e8d949e34cb23d2bb0ed0c960c26915fef7b
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2023-05-13 10:55:56 +0200

    Update

--- lambda/IntentHandlers/MakeReservationIntent.js
+++ lambda/IntentHandlers/MakeReservationIntent.js
@@ -47,7 +47,7 @@ const CompletedMakeReservationIntentHandler = {
 	},
 	handle(handlerInput) {
 		const speakOutput = "Reservation completed."
-		return handlerInput.responseBuilder.speak(speakOutput).getResponse()
+		return handlerInput.responseBuilder.speak(speakOutput).withShouldEndSession(true).getResponse()
 	},
 }
 
