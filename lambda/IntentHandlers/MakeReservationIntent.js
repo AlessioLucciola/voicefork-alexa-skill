7853ff38878ceda1df91e9746c10e0fcb8ff83b2
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2023-05-13 12:38:11 +0200

    Update

--- lambda/IntentHandlers/MakeReservationIntent.js
+++ lambda/IntentHandlers/MakeReservationIntent.js
@@ -4,19 +4,32 @@ const { format } = require("date-fns")
 const StartedMakeReservationIntentHandler = {
 	canHandle(handlerInput) {
 		const { dialogState, type, name } = handlerInput.requestEnvelope.request
-		return type === "IntentRequest" && name === "MakeReservationIntent" && dialogState == "IN_PROGRESS"
+		return type === "IntentRequest" && name === "MakeReservationIntent" && dialogState !== "COMPLETED"
 	},
 	handle(handlerInput) {
-		const currentIntent = handlerInput.requestEnvelope.request.intent
+		return handlerInput.responseBuilder
+			.speak("What is the name of the place?")
+			.reprompt("Please, tell me the name of the place you want to make a reservation for")
+			.addDelegateDirective()
+			.getResponse()
+	},
+}
+
+const VerifyRestaurantNameReservationHandler = {
+	canHandle(handlerInput) {
+		const { dialogState, type, name } = handlerInput.requestEnvelope.request
+		const { restauantName } = handlerInput.requestEnvelope.request.intent.slots
+		return type === "IntentRequest" && name === "MakeReservationIntent" && restauantName.value
+	},
+	handle(handlerInput) {
 		const { restauantName } = request.intent.slots
 
-		if (restauantName.value == "marione") return handlerInput.responseBuilder.speak("Ok, marioneeeee").addDelegateDirective(currentIntent).getResponse()
+		if (restauantName.value == "marione") return handlerInput.responseBuilder.speak("Ok, marioneeeee").addDelegateDirective().getResponse()
 
 		return handlerInput.responseBuilder
 			.speak("The restaurant name is not valid (you have to say marione!) What is the name of the place?")
 			.reprompt("Please, tell me the name of the place you want to make a reservation for")
 			.addElicitSlotDirective("restaurantName")
-			.addDelegateDirective(currentIntent)
 			.getResponse()
 	},
 }
