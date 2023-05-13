5d092d65185e201dc2042058c9a89afbcc38567f
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2023-05-13 05:52:40 +0000

    updated lambda/defaultIntents.js.

--- lambda/defaultIntents.js
+++ lambda/defaultIntents.js
@@ -1,16 +1,16 @@
 module.export = {
-    launchRequestHandler {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
-    },
-    handle(handlerInput) {
-        const speakOutput = 'Hi, tell me the details of the reservation!';
-
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
-            .getResponse();
-    }
+    const launchRequestHandler = {
+        canHandle(handlerInput) {
+            return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+        },
+        handle(handlerInput) {
+            const speakOutput = 'Hi, tell me the details of the reservation!';
+    
+            return handlerInput.responseBuilder
+                .speak(speakOutput)
+                .reprompt(speakOutput)
+                .getResponse();
+        }
 },
 helloWorldIntentHandler {
     canHandle(handlerInput) {
