a759d68177c1fd98314faa0b192e231c8c849734
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2023-05-13 06:38:33 +0000

    updated lambda/customIntents.js.

--- lambda/customIntents.js
+++ lambda/customIntents.js
@@ -1,12 +1,46 @@
 const Alexa = require('ask-sdk-core');
 const { format } = require('date-fns');
 
-const StartMakeReservationIntentHandler = {
+const StaredtMakeReservationIntentHandler = {
     canHandle(handlerInput) {
         const request = handlerInput.requestEnvelope.request;
         return request.type === 'IntentRequest' &&
-          request.intent.name === 'PlanMyTripIntent' &&
+          request.intent.name === 'MakeReservationIntent' &&
           request.dialogState === 'STARTED';
+        },
+    // canHandle(handlerInput) {
+    //     return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+    //         && Alexa.getIntentName(handlerInput.requestEnvelope) === 'MakeReservationIntent';
+    // },
+    handle(handlerInput) {
+    const speakOutput = 'Tell me the details of the reservatio.';
+    return handlerInput.responseBuilder.speak(speakOutput).getResponse();
+  }
+};
+
+const InProgressMakeReservationIntentHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest' &&
+          request.intent.name === 'MakeReservationIntent' &&
+          request.dialogState === 'IN_PROGRESS';
+        },
+    // canHandle(handlerInput) {
+    //     return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+    //         && Alexa.getIntentName(handlerInput.requestEnvelope) === 'MakeReservationIntent';
+    // },
+    handle(handlerInput) {
+    const speakOutput = 'Tell me the details of the reservatio.';
+    return handlerInput.responseBuilder.speak(speakOutput).getResponse();
+  }
+};
+
+const CompletedMakeReservationIntentHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest' &&
+          request.intent.name === 'MakeReservationIntent' &&
+          request.dialogState === 'COMPLETED';
         },
     // canHandle(handlerInput) {
     //     return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
