47b14565c2a73ee97342d3faef7b6e8f9cf65cd7
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2023-05-13 05:58:02 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -25,10 +25,7 @@ const MakeReservationIntentHandler = {
             //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
             .getResponse();
     }
-}
-
-
-
+};
 
 
 /* *
