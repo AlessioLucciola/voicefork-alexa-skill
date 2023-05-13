740008edb487c3c0916e9c745f082c8767383a30
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2023-05-13 06:37:25 +0000

    updated lambda/customIntents.js.

--- lambda/customIntents.js
+++ lambda/customIntents.js
@@ -4,34 +4,19 @@ const { format } = require('date-fns');
 const MakeReservationIntentHandler = {
     
     canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
-    return request.type === 'IntentRequest' &&
-      request.intent.name === 'PlanMyTripIntent' &&
-      request.dialogState === 'IN_PROGRESS';
-  },
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest' &&
+          request.intent.name === 'PlanMyTripIntent' &&
+          request.dialogState === 'IN_PROGRESS';
+        },
     // canHandle(handlerInput) {
     //     return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
     //         && Alexa.getIntentName(handlerInput.requestEnvelope) === 'MakeReservationIntent';
     // },
     handle(handlerInput) {
-    const { request } = handlerInput.requestEnvelope;
-    const { intent } = request;
-    const { slots } = intent;
+    
+     const speakOutput = 'Tell me the date of the reservation.';
 
-    const dateSlotValue = slots['date'].value;
-
-    if (!dateSlotValue) {
-      // Date slot not yet filled
-      const speakOutput = 'Tell me the date of the reservation.';
-      return handlerInput.responseBuilder.speak(speakOutput).getResponse();
-    }
-
-    // Date slot is filled
-    const date = new Date(dateSlotValue);
-    const formattedDate = format(date, 'EEEE, MMMM do, yyyy');
-    const speakOutput = `The date of the reservation is ${formattedDate}. Now, please provide the remaining details.`;
-
-    // Handle the remaining reservation details here
 
     return handlerInput.responseBuilder.speak(speakOutput).getResponse();
   }
