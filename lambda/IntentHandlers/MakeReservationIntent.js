eff438e1dfae58a0b17c4fb078cc4382127afc61
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2023-05-13 13:54:36 +0200

    Update

--- lambda/IntentHandlers/MakeReservationIntent.js
+++ lambda/IntentHandlers/MakeReservationIntent.js
@@ -8,7 +8,7 @@ const StartedMakeReservationIntentHandler = {
 		return type === "IntentRequest" && name === "MakeReservationIntent" && dialogState !== "COMPLETED"
 	},
 	handle(handlerInput) {
-		return handlerInput.responseBuilder.addDelegateDirective().getResponse()
+		return handlerInput.responseBuilder.prompt("Starting").addDelegateDirective().getResponse()
 	},
 }
 
