823cb120a217009f775cfeb2f73960c458ccdabe
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2023-05-13 06:24:56 +0000

    updated lambda/customIntents.js.

--- lambda/customIntents.js
+++ lambda/customIntents.js
@@ -1,4 +1,5 @@
 const Alexa = require('ask-sdk-core');
+const { format } = require('date-fns');
 
 const MakeReservationIntentHandler = {
     canHandle(handlerInput) {
@@ -7,7 +8,12 @@ const MakeReservationIntentHandler = {
     },
     handle(handlerInput) {
         const speakOutput = 'Hi, tell me the details of the reservation!';
+        
+        const currentDate = Date()
+        const formattedDate = format(currentDate, 'EEEE, MMMM do, yyyy');
+        const speechOutput = `Today is ${formattedDate}.`;
 
+        return handlerInput.responseBuilder.speak(speechOutput).getResponse();
         return handlerInput.responseBuilder
             .speak(speakOutput)
             //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
