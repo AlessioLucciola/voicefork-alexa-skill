0532537e2afd13406c2f031988c64bbcd05f722b
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2023-05-13 05:56:00 +0000

    updated lambda/defaultIntents.js.

--- lambda/defaultIntents.js
+++ lambda/defaultIntents.js
@@ -1,7 +1,6 @@
 const Alexa = require('ask-sdk-core');
 
-module.exports = {
-    LaunchRequestHandler = {
+const LaunchRequestHandler = {
         canHandle(handlerInput) {
             return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
         },
@@ -14,11 +13,8 @@ module.exports = {
                 .getResponse();
         }
 }
-}
 
-exports.
-
-exports.HelloWorldIntentHandler = {
+const HelloWorldIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
@@ -31,4 +27,10 @@ exports.HelloWorldIntentHandler = {
             //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
             .getResponse();
     }
+}
+
+
+module.exports = {
+    LaunchRequestHandler,
+    HelloWorldIntentHandler
 }
