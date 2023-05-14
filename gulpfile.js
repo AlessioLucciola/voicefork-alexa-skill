551c4ffc55938a9653b47a70da24c0623022b3f4
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2023-05-14 15:25:42 +0200

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp = require("gulp")
 var ts = require("gulp-typescript")
 var tsProject = ts.createProject("tsconfig.json")
-var OUT_DIR = "lambda"
-var IN_DIR = "src"
+var OUT_DIR = "dist"
+var IN_DIR = "lambda"
 // compile typescript
 gulp.task("compile", function () {
 	return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest(OUT_DIR))
--- lambda/apiCalls.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.searchNearbyRestaurants = void 0;
-const searchNearbyRestaurants = (query, latitude, longitude) => {
-    return;
-};
-exports.searchNearbyRestaurants = searchNearbyRestaurants;
