b4efb048e442b4c08b18832f115dfdd768e4a839
Anomalyzed <corsi.1742375@studenti.uniroma1.it>
2023-06-11 16:36:25 +0200

    Updating

--- lambda/shared/urls.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RESERVATIONS_URL = exports.USERS_URL = exports.RESTAURANTS_URL = void 0;
-// This has to be changed each time ngrok is restarted
-const NGROK_URL = 'https://614f-94-34-145-237.eu.ngrok.io';
-exports.RESTAURANTS_URL = `${NGROK_URL}/restaurants`;
-exports.USERS_URL = `${NGROK_URL}/users`;
-exports.RESERVATIONS_URL = `${NGROK_URL}/reservations`;
--- lambda/utils/debugUtils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.beautify = void 0;
-const beautify = (object) => {
-    return JSON.stringify(object, null, 2);
-};
-exports.beautify = beautify;
